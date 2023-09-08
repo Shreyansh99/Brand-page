4d2544527cd44c529fc0c61b99be4f9c4f757490
unknown <shreyanshkumarbasak@gmail.com>
2023-09-08 19:35:18 +0530

    done

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import Navigation from '@/public/components/Navigation'
+import Hero from '@/public/components/Hero'
+
+const page = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default page
---
+++ public/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav>
+    <div>
+    <img src="/images/brand_logo.png" alt="logo" />
+    </div>
+    <ul>
+      <li >Menu</li>
+      <li>Loaction</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+    <button>Login</button>
+
+  </nav>
+  )
+}
+
+export default Navigation
